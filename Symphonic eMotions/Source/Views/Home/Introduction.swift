c4e8938aa077305a22dcdd19b2d6d9fff6e56bd5
Frans-Jan Wind <82514432+Symphonic-eMotions@users.noreply.github.com>
2023-06-22 14:06:13 +0200

    Back button

--- Symphonic eMotions/Source/Views/Home/Introduction.swift
+++ Symphonic eMotions/Source/Views/Home/Introduction.swift
@@ -61,7 +61,7 @@ class FrameExtractorViewModel: FrameExtractorDelegate, ObservableObject {
 
 struct Introduction: View {
     
-    @AppStorage(UserDefaultsKeys.currentUrl) var currentUrl: String = "PlayListsView"
+    @AppStorage(UserDefaultsKeys.currentUrl) var currentUrl: String = "Introduction"
     
     @ObservedObject var setInfoModel: SetInfoModel
     @Binding public var sessionDisplay: SessionDisplay
@@ -70,179 +70,244 @@ struct Introduction: View {
     @ObservedObject var frameExtractorViewModel = FrameExtractorViewModel()
     
     @State var avgWhiteValue: Double = 0
-    @State private var introdutiePlaying: Bool = false
-    internal var introductionNoteNumbers: [Int] = [36,38,40,41,43,57,59,48]
+    @State var whiteTestResult: String = "? %"
+    @State private var testSoundPlaying: Bool = false
+    internal var testSoundNoteNumbers: [Int] = [36,38,40,41,43,57,59,48]
     
     var body: some View {
         
-        VStack(spacing: 0) {
+        ZStack(alignment: .topLeading){
             
-            if sessionDisplaySub == .page01 {
+            VStack(spacing: 0) {
                 
-                IntroductionImage(
-                    imageName: "page01",
-                    customWidth: 0.9,
-                    customHeight: 0.7
-                )
-                
-                Spacer()
-                
-                Text(" ")
-                    .padding()
-                
-                Spacer()
-                
-                IntroductionTitle(
-                    setInfoModel: setInfoModel,
-                    sessionDisplay: $sessionDisplay,
-                    sessionDisplaySub: $sessionDisplaySub,
-                    localizedString: "iPad in stand",
-                    nextPage: .page02,
-                    introductionNoteNumbers: []
-                )
-                
-                Spacer()
-            }
-            
-            else if sessionDisplaySub == .page02 {
-                
-                IntroductionImage(
-                    imageName: "page02",
-                    customWidth: 0.8,
-                    customHeight: 0.6
-                )
-                
-                Spacer()
-                
-                VStack{
-                    //Start stop
+                //Standaard
+                if sessionDisplaySub == .page01 {
                     
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 220, height: 60)
-                            .foregroundColor(.clear)
-                            .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
-                            .background( Color.accentColor )
+                    IntroductionImage(
+                        imageName: "page01",
+                        customWidth: 0.9,
+                        customHeight: 0.7
+                    )
+                    
+                    Spacer()
+                    
+                    Text(" ")
+                        .padding()
+                    
+                    Spacer()
+                    
+                    IntroductionTitle(
+                        setInfoModel: setInfoModel,
+                        sessionDisplay: $sessionDisplay,
+                        sessionDisplaySub: $sessionDisplaySub,
+                        localizedString: "iPad in stand",
+                        nextPage: .page02,
+                        introductionNoteNumbers: []
+                    )
+                    
+                    Spacer()
+                }
+                //Volume
+                else if sessionDisplaySub == .page02 {
+                    
+                    IntroductionImage(
+                        imageName: "page02",
+                        customWidth: 0.8,
+                        customHeight: 0.6
+                    )
+                    
+                    Spacer()
+                    
+                    VStack{
+                        //Start stop
                         
-                        Text( introdutiePlaying ?
-                              NSLocalizedString("Audio is playing", comment: "") :
-                                NSLocalizedString("Test audio", comment: "")
-                        )
-                        .font(.system(size: 30))
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 220, height: 60)
+                                .foregroundColor(.clear)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+                                .background( Color.accentColor )
+                            
+                            Text( testSoundPlaying ?
+                                  NSLocalizedString("Audio is playing", comment: "") :
+                                    NSLocalizedString("Test audio", comment: "")
+                            )
+                            .font(.system(size: 30))
+                            .padding()
+                        }
+                        .onTapGesture {
+                            //Direct connection Introductie set
+                            setInfoModel.conductor.playNoteNumbersIntroduction(
+                                trackId: "realLife",
+                                soundSource: .audioBuffer,
+                                noteNumbers: testSoundNoteNumbers,
+                                noteOn: testSoundPlaying
+                            )
+                            
+                            testSoundPlaying.toggle()
+                        }
+                        
+                        //Volume
+                        VStack(alignment: .leading){
+                            VolumeSlider()
+                                .frame(width: UIScreen.main.bounds.width * 0.6, height: 10)
+                                .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+                                .zIndex(101)
+                        }
+                    }
+                    Spacer()
+                    
+                    IntroductionTitle(
+                        setInfoModel: setInfoModel,
+                        sessionDisplay: $sessionDisplay,
+                        sessionDisplaySub: $sessionDisplaySub,
+                        localizedString: "Connect audio",
+                        nextPage: .page03,
+                        introductionNoteNumbers: testSoundNoteNumbers
+                    )
+                    
+                    Spacer()
+                }
+                //Licht
+                else if sessionDisplaySub == .page03 {
+                    
+                    IntroductionImage(
+                        imageName: "page03",
+                        customWidth: 0.8,
+                        customHeight: 0.6
+                    )
+                    
+                    Spacer()
+                    
+                    HStack{
+                        
+                        //Test licht
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 220, height: 60)
+                                .foregroundColor(.clear)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+                                .background( Color.accentColor )
+                            
+                            Text( NSLocalizedString("Text light", comment: ""))
+                                .font(.system(size: 30))
+                                .padding()
+                        }
+                        .onTapGesture {
+                            //Do white average calculation here
+                            self.avgWhiteValue = self.frameExtractorViewModel.calculateAvgWhiteValue(self.frameExtractorViewModel.image ?? CIImage())
+                            whiteTestResult = "\(Int(avgWhiteValue/2.55))%"
+                            
+                            print("Connect to sensitivity")
+                            
+                        }
+                        .padding()
+                        
+                        HStack {
+                            Rectangle()
+                                .fill(Color.black)
+                                .frame(width: 100, height: 100)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+                            ZStack{
+                                Rectangle()
+                                    .fill(Color(
+                                        red: avgWhiteValue / 255.0,
+                                        green: avgWhiteValue / 255.0,
+                                        blue: avgWhiteValue / 255.0
+                                    ))
+                                    .frame(width: 100, height: 100)
+                                    .border(Color.blue, width: 2)
+                                    .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+                                Text(whiteTestResult)
+                                    .font(.system(size: 30))
+                            }
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: 100, height: 100)
+                                .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+                        }
                         .padding()
                     }
-                    .onTapGesture {
-                        //Direct connection Introductie set
-                        setInfoModel.conductor.playNoteNumbersIntroduction(
-                            trackId: "realLife",
-                            soundSource: .audioBuffer,
-                            noteNumbers: introductionNoteNumbers,
-                            noteOn: introdutiePlaying
-                        )
-                        
-                        introdutiePlaying.toggle()
-                    }
                     
-                    //Volume
-                    VStack(alignment: .leading){
-                        VolumeSlider()
-                            .frame(width: UIScreen.main.bounds.width * 0.6, height: 10)
-                            .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
-                            .zIndex(101)
-                    }
+                    Spacer()
+                    
+                    IntroductionTitle(
+                        setInfoModel: setInfoModel,
+                        sessionDisplay: $sessionDisplay,
+                        sessionDisplaySub: $sessionDisplaySub,
+                        localizedString: "Enough light",
+                        nextPage: .page04,
+                        introductionNoteNumbers: []
+                    )
+                    
+                    Spacer()
                 }
-                Spacer()
                 
-                IntroductionTitle(
-                    setInfoModel: setInfoModel,
-                    sessionDisplay: $sessionDisplay,
-                    sessionDisplaySub: $sessionDisplaySub,
-                    localizedString: "Connect audio",
-                    nextPage: .page03,
-                    introductionNoteNumbers: introductionNoteNumbers
-                )
-                
-                Spacer()
+                else if sessionDisplaySub == .page04 {
+                    
+                    IntroductionImage(
+                        imageName: "page04",
+                        customWidth: 1.0,
+                        customHeight: 0.8
+                    )
+                    
+                    Spacer()
+                    
+                    Text("Set feedback")
+                    
+                    Spacer()
+                    
+                    IntroductionTitle(
+                        setInfoModel: setInfoModel,
+                        sessionDisplay: $sessionDisplay,
+                        sessionDisplaySub: $sessionDisplaySub,
+                        localizedString: "Specift distance",
+                        nextPage: .none,
+                        introductionNoteNumbers: []
+                    )
+                    
+                    Spacer()
+                }
+            }
+            .onAppear{
+                //Load introduction set
+                if currentUrl != "Introductie.json" {
+                    //Load set
+                    setInfoModel.tapSetRow(filePath: "Introductie.json")
+                    //Let @AppStorage know what is current
+                    currentUrl = "Introductie.json"
+                }
             }
             
-            else if sessionDisplaySub == .page03 {
-                
-                IntroductionImage(
-                    imageName: "page03",
-                    customWidth: 0.8,
-                    customHeight: 0.6
-                )
-                
-                Spacer()
+            //Back button
+            if sessionDisplaySub != .page01 {
                 
                 ZStack {
-                    Rectangle()
-                        .frame(width: 220, height: 60)
-                        .foregroundColor(.clear)
-                        .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
-                        .background( Color.accentColor )
                     
-                    Text( NSLocalizedString("Text light", comment: ""))
+                    Image(systemName: "arrowshape.backward")
+                    .font(.system(size: 40))
+                    .foregroundColor(.blue)
                     
-                        .font(.system(size: 30))
-                        .padding()
+//                    Rectangle()
+//                        .frame(width: 200, height: 60)
+//                        .foregroundColor(.clear)
+//                        .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(.white))
+//                        .background( Color.accentColor )
+//
+//                    Text(NSLocalizedString("Back", comment: ""))
+//                        .font(.system(size: 30))
+//                        .padding()
                 }
+                .padding(.top, 20)
+                .padding(.leading, 20)
                 .onTapGesture {
-                    
-                    //Do white average calculation here
-                    self.avgWhiteValue = self.frameExtractorViewModel.calculateAvgWhiteValue(self.frameExtractorViewModel.image ?? CIImage())
-                    print(self.avgWhiteValue)
+                    withAnimation {
+                        let pages:[SessionDisplay:SessionDisplay] = [.page02:.page01,.page03:.page02,.page04:.page03]
+                        if let prevPage = pages[sessionDisplaySub] {
+                            sessionDisplaySub = prevPage
+                        }
+                    }
                 }
-                
-                Spacer()
-                
-                IntroductionTitle(
-                    setInfoModel: setInfoModel,
-                    sessionDisplay: $sessionDisplay,
-                    sessionDisplaySub: $sessionDisplaySub,
-                    localizedString: "Enough light",
-                    nextPage: .page04,
-                    introductionNoteNumbers: []
-                )
-                
-                Spacer()
-            }
-            
-            else if sessionDisplaySub == .page04 {
-                
-                IntroductionImage(
-                    imageName: "page04",
-                    customWidth: 0.8,
-                    customHeight: 0.6
-                )
-                
-                Spacer()
-                
-                Text("Set feedback")
-                
-                Spacer()
-                
-                IntroductionTitle(
-                    setInfoModel: setInfoModel,
-                    sessionDisplay: $sessionDisplay,
-                    sessionDisplaySub: $sessionDisplaySub,
-                    localizedString: "Specift distance",
-                    nextPage: .none,
-                    introductionNoteNumbers: []
-                )
-                
-                Spacer()
-            }
-            
-            
-        }
-        .onAppear{
-            
-            if currentUrl != "Introductie.json" {
-                //Load set
-                setInfoModel.tapSetRow(filePath: "Introductie.json")
-                //Let @AppStorage know what is current
-                currentUrl = "Introductie.json"
             }
         }
     }
@@ -264,12 +329,11 @@ struct IntroductionImage: View {
             Image(imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: imageWidth, height: imageHeight, alignment: .topLeading)
+                .frame(width: imageWidth, height: imageHeight, alignment: .center)
             
             Spacer()
         }
         .padding(.top, 40)
-        //        .border(.red)
     }
 }
 
@@ -304,7 +368,7 @@ struct IntroductionTitle: View {
             }
             .onTapGesture {
                 withAnimation {
-                    
+                    //Shit down audio test notes
                     if nextPage == .page03 {
                         setInfoModel.conductor.playNoteNumbersIntroduction(
                             trackId: "realLife",
@@ -314,15 +378,15 @@ struct IntroductionTitle: View {
                         )
                     }
                     
+                    //At the end of the introduction go to the demo
                     if nextPage == .none {
                         sessionDisplay = .demo
                     }
-                    else{
+                    else {
                         sessionDisplaySub = nextPage
                     }
                 }
             }
-            
             Spacer()
         }
         .padding(.bottom)
