0cdbfb68732584b7f6f9c5a7afdab2febbc57205
Symphonic eMotions <info@symphonic-emotions.nl>
2024-04-26 00:35:18 +0200

    Deployment target to 16.6
    Last SpriteKit clean up

--- Aantekeningen.swift
+++ Aantekeningen.swift
@@ -1,1 +1,73 @@
+import SwiftUI
+
+struct MainView: View {
+    
+    @ObservedObject var setInfoModel: SetInfoModel
+    
+    var body: some View {
+            
+        NavigationView {
+            
+            //Set Navigation
+            SideBarView(
+                setInfoModel: setInfoModel
+            )
+            //TODO: Hide SideBar on fullscreen
+//            .navigationBarHidden(showLevelPlayerFullScreen)
+
+            SetInfo(
+                setInfoModel: setInfoModel
+            )
+        }
+        .navigationViewStyle(DoubleColumnNavigationViewStyle())
+    }
+}
+
+struct SideBarView: View {
+
+    @ObservedObject var setInfoModel: SetInfoModel
+    
+    init(
+        setInfoModel: SetInfoModel
+        
+    ) {
+        self.setInfoModel = setInfoModel
+    }
+    
+    var body: some View {
+        
+        //Making this conditional (if showNavigation) leaves a blanc space
+        NavigationView {
+            List {
+                //Main menu itemms
+                ForEach(sidebarItems, id: \.setName) { item in
+                    Button(action: {}) {
+                        SidebarItemView(
+                            item: item,
+                            active: selectedMainItem == item.sessionDisplay,
+                            showDisabled: $showDisabled
+                        )
+                    }
+                }
+                
+            }
+            .navigationTitle(setInfoModel.setInfoLocalState.sideBarHead)
+        }
+    }
+}
+
+struct SetInfo: View {
+    
+    @ObservedObject var setInfoModel: SetInfoModel
+    
+    var body: some View {
+        VStack{
+            Button(action: {
+                //What to call here to hide NavigationView in it's native way
+            }, {
+                Text("Close navigation")
+            }
+        }
+    }
+}
 
